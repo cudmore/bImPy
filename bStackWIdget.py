b4c75f8d01be807b77aca2fae3af5734d1512f12
cudmore <robertcudmore@gmail.com>
2019-08-05 13:07:44 -0700

    to nathan

--- bStackWIdget.py
+++ bStackWIdget.py
@@ -28,33 +28,33 @@ from matplotlib.backends import backend_qt5agg
 
 class bPointList:
 	def __init__(self, tifPath):
-		
+
 		self.x = None
 		self.y = None
 		self.z = None
-		
+
 		self.edgeList = []
-		
+
 		pointFilePath, ext = os.path.splitext(tifPath)
 		pointFilePath += '.txt'
-		
+
 		if not os.path.isfile(pointFilePath):
 			print('bPointList error, did not find', pointFilePath)
 			return
 		else:
 			df = pd.read_csv(pointFilePath)
-			
+
 			self.x = df.iloc[:,0].values
 			self.y = df.iloc[:,1].values
 			self.z = df.iloc[:,2].values
-			
+
 			print('tracing z max:', np.nanmax(self.z))
-			
+
 		self.analyze()
-		
+
 	def numPoints(self):
 		return len(self.x)
-	
+
 	def analyze(self):
 		edgeIdx = 0
 		edgeDict = {'Idx':edgeIdx, 'n':'', 'Length':''}
@@ -68,9 +68,9 @@ class bPointList:
 				self.edgeList.append(edgeDict)
 				continue
 			edgeDict['Idx'] = edgeIdx
-		
+
 		#print(self.edgeList)
-		
+
 class bAnnotationTable(QtWidgets.QWidget):
 	def __init__(self, mainWindow=None, parent=None, pointList=None):
 		super(bAnnotationTable, self).__init__(parent)
@@ -85,11 +85,11 @@ class bAnnotationTable(QtWidgets.QWidget):
 		self.myTableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
 		self.myTableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
 		self.myTableWidget.cellClicked.connect(self.on_clicked)
-	
+
 		headerLabels = ['x', 'y', 'z']
 		self.myTableWidget.setHorizontalHeaderLabels(headerLabels)
 
-		header = self.myTableWidget.horizontalHeader()	   
+		header = self.myTableWidget.horizontalHeader()
 		header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
 		# QHeaderView will automatically resize the section to fill the available space.
 		# The size cannot be changed by the user or programmatically.
@@ -99,13 +99,13 @@ class bAnnotationTable(QtWidgets.QWidget):
 			stat = str(stat)
 			item = QtWidgets.QTableWidgetItem(stat)
 			self.myTableWidget.setItem(idx, 0, item)
-		
+
 			item = QtWidgets.QTableWidgetItem(stat)
 			self.myTableWidget.setItem(idx, 1, item)
-			
+
 			item = QtWidgets.QTableWidgetItem(stat)
 			self.myTableWidget.setItem(idx, 2, item)
-			
+
 		self.myQVBoxLayout.addWidget(self.myTableWidget)
 
 	@QtCore.pyqtSlot()
@@ -118,7 +118,7 @@ class bAnnotationTable(QtWidgets.QWidget):
 		yStat = self.myTableWidget.item(row,0).text() #
 		print('   ', row, yStat)
 		#self.myParent.metaPlotStat(yStat)
-	
+
 #class bStackWindow(FigureCanvas):
 #	def __init__(self, parent=None, path=''):
 class bStackWidget(QtWidgets.QWidget):
@@ -128,20 +128,20 @@ class bStackWidget(QtWidgets.QWidget):
 
 		basename = os.path.basename(self.path)
 		self.setWindowTitle(basename)
-		
+
 		self.setAcceptDrops(True)
-		
+
 		self.loadStack()
-				
+
 		self.currentSlice = 0
 		self.voxelx = 1
 		self.voxely = 1
 		self.imgplot = None
-		
+
 		dpi = 100
 		width = 12
 		height = 12
-		
+
 		self.myHBoxLayout = QtWidgets.QHBoxLayout(self)
 
 		# image
@@ -156,7 +156,7 @@ class bStackWidget(QtWidgets.QWidget):
 		self.myScatterPlot = self.axes.scatter([], [], marker='o', color='y', s=markersize, picker=True)
 		self.annotationTable = bAnnotationTable(mainWindow=None, parent=None, pointList=self.pointList)
 		self.myHBoxLayout.addWidget(self.annotationTable, stretch=2) # stretch=10, not sure on the units???
-		
+
 
 		#FigureCanvas.__init__(self, self.figure)
 		#self.setParent(parent)
@@ -175,7 +175,7 @@ class bStackWidget(QtWidgets.QWidget):
 		self.setFocusPolicy(QtCore.Qt.ClickFocus)
 		self.setFocus()
 		'''
-		
+
 	def dragEnterEvent(self, event):
 		#print('dragEnterEvent:', event)
 		if event.mimeData().hasUrls:
@@ -199,26 +199,26 @@ class bStackWidget(QtWidgets.QWidget):
 				print('   ', url.toLocalFile())
 		else:
 			event.ignore()
-		
+
 
 	def setSlice(self, index):
-		print('setSlice:', index)
-		
+		#print('setSlice:', index)
+
 		if index < 0:
 			index = 0
 		if index > self.numSlices-1:
 			index = self.numSlices -1
-		
+
 		iLeft = 0
 		iTop = 0
 		iRight = self.voxelx * self._images.shape[2] # reversed
 		iBottom = self.voxely * self._images.shape[1]
 
 		if self.imgplot is None:
-			self.imgplot = self.axes.imshow(self._images[index,:,:], cmap='Greens_r', extent=[iLeft, iRight, iBottom, iTop])  # l, r, b, t		
+			self.imgplot = self.axes.imshow(self._images[index,:,:], cmap='Greens_r', extent=[iLeft, iRight, iBottom, iTop])  # l, r, b, t
 		else:
 			self.imgplot.set_data(self._images[index,:,:])
-			
+
 		#
 		# update point annotations
 		showTracing = True
@@ -232,14 +232,14 @@ class bStackWidget(QtWidgets.QWidget):
 				self.yMasked = self.pointList.x[~self.zMask.mask]
 			#except:
 			#	print('ERROR: bStackWindow.setSlice')
-			
+
 			self.myScatterPlot.set_offsets(np.c_[self.xMasked, self.yMasked])
-		
+
 		self.currentSlice = index # update slice
-		
+
 		self.canvas.draw()
 		self.repaint() # this is updating the widget !!!!!!!!
-		
+
 	@property
 	def numSlices(self):
 		if self._images is not None:
@@ -267,28 +267,31 @@ class bStackWidget(QtWidgets.QWidget):
 		elif event.button == 'down':
 			self.currentSlice += 1
 		self.setSlice(self.currentSlice)
-		
+
 	def onpick(self, event):
 		print('bStackWindow.onpick()', event.ind)
-	
+
 if __name__ == '__main__':
 	import sys
-	
+
 	#path = '/Users/cudmore/box/DeepVess/data/invivo/20190613__0028.tif'
-	path = '/Users/cudmore/box/DeepVess/data/mytest.tif'
-	
+	if len(sys.argv) == 2:
+		path = sys.argv[1]
+	else:
+		path = '/Users/cudmore/box/DeepVess/data/mytest.tif'
+
 	app = QtWidgets.QApplication(sys.argv)
 
 	sw = bStackWidget(mainWindow=app, parent=None, path=path)
 	sw.show()
 	sw.setSlice(0)
-	
+
 	'''
 	sw2 = bStackWidget(mainWindow=app, parent=None, path=path)
 	sw2.show()
 	sw2.setSlice(0)
 	'''
-	
+
 	print('app.topLevelWidgets():', app.topLevelWidgets())
-	
+
 	sys.exit(app.exec_())
