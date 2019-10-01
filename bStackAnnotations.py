fe28bcf1626212590899327383a1e4cd519c2112
cudmore <robertcudmore@gmail.com>
2019-09-30 17:15:37 -0700

    annotations

---
+++ bStackAnnotations.py
@@ -0,0 +1,24 @@
+# Author: Robert Cudmore
+# Date: 20190930
+
+"""
+Manage annotations for one stack
+"""
+
+class bStackAnnotations
+
+	def __init__(self, stack):
+		self._stack = stack
+		self._annotation = []
+
+	def save(self):
+		pass
+
+	def load(self):
+		pass
+
+	def importRoiManager(self, zipFile):
+		"""
+		Import annotations from Fiji ROI .zip file
+		"""
+		pass
