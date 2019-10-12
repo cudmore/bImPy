8fc58cc9fc130a715209d0d35541feca139200ff
cudmore <robert.cudmore@gmail.com>
2019-10-12 10:18:25 -0700

    moved stuff around

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+exec (open('bimpy/version.py').read())
+
+setup(
+    name='bimpy',
+    version=__version__,
+    description='Image volume and time series analysis',
+    url='http://github.com/cudmore/bImPy',
+    author='Robert H Cudmore',
+    author_email='robert.cudmore@gmail.com',
+    license='GNU GPLv3',
+    packages = find_packages(),
+    #packages = find_packages(exclude=['version']),
+    #packages=[
+    #    'pymapmanager',
+    #    'pymapmanager.mmio'
+    #],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'tifffile',
+        'PyQt5',
+        'PyQtChart',
+        'scikit-image',
+        'javabridge',
+        'python-bioformats',
+    ]
+)
