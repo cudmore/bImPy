337b1cc2ba276571968169d8f0074efcd136a357
cudmore <robert.cudmore@gmail.com>
2020-07-31 14:14:28 -0700

    lots

---
+++ examples/edt/vascularDistanceMap_batch.sh
@@ -0,0 +1,3 @@
+python vascularDistanceMap.py '/Users/cudmore/box/data/nathan/20200420/20200420_HEAD_.oir'
+python vascularDistanceMap.py '/Users/cudmore/box/data/nathan/20200420/20200420_MID_.oir'
+python vascularDistanceMap.py '/Users/cudmore/box/data/nathan/20200420/20200420_distalHEAD_.oir'
